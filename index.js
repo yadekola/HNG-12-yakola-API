a355eb924e2caaed4c4d667f49c8a4226c86d3fa
yadekola <yakubadekolaojo059@gmail.com>
2025-02-02 14:47:45 +0100

    first Update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
   const response = {
     email: "yakubadekolaojo059@gmail.com", 
     current_datetime: new Date().toISOString(), 
-    github_url: "https://github.com/yadekola/HNG-12-API" 
+    github_url: "https://github.com/yadekola/HNG-12-yakola-API" 
   };
 
   res.status(200).json(response);
